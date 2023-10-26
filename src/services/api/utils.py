67a07c2b1d5c495339cce4f51802639176b012f4
GGduisheevArturGG <107888994+ArturDuisheev@users.noreply.github.com>
2023-10-26 22:41:40 +0600

    fix errors

--- src/services/api/utils.py
+++ src/services/api/utils.py
@@ -2,9 +2,16 @@ from PIL import Image
 from io import BytesIO
 
 
+def convert_image_to_rgb(image):
+    if image.mode != 'RGB':
+        image = image.convert('RGB')
+    return image
+
+
 def compress_image(image):
     try:
         img = Image.open(image)
+        img = convert_image_to_rgb(img)
         img.thumbnail((800, 800))
         output_buffer = BytesIO()
         img.save(output_buffer, format='JPEG', quality=30)
